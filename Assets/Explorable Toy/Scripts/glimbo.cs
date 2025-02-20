28d03196cef7814db9a177ddaa09ea4f2fc34f85
jhgh <92657664+stapleshgh@users.noreply.github.com>
2025-02-19 19:00:44 -0500

    glimbos now orbit the mouse

---
+++ Assets/Explorable Toy/Scripts/glimbo.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glimbo : MonoBehaviour
+{
+
+    Vector3 mousePos;
+    public float acceleration;
+    public float speed;
+    public Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+        
+
+
+        approachCursor();
+
+
+        transform.position += velocity;
+
+
+
+        
+    }
+
+    void approachCursor()
+    {
+        if (transform.position != mousePos)
+        {
+            //x direction velocity
+            if (transform.position.x < mousePos.x)
+            {
+                velocity.x += speed * Time.deltaTime * Vector2.Distance(transform.position, mousePos);
+            } 
+            else if (transform.position.x > mousePos.x)
+            {
+                velocity.x -= speed * Time.deltaTime * Vector2.Distance(transform.position, mousePos);
+            }
+
+            //y direction velocity
+            if (transform.position.y < mousePos.y)
+            {
+                velocity.y += speed * Time.deltaTime * Vector2.Distance(transform.position, mousePos);
+            }
+            else if (transform.position.y > mousePos.y)
+            {
+                velocity.y -= speed * Time.deltaTime * Vector2.Distance(transform.position, mousePos);
+            }
+
+        }
+    }
+}
